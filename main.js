6e24397453b0dc6bde8727da35259a6a722a567d
hendrixombonyo <ombonyohendrix@zetech.ac.ke>
2023-03-11 22:41:21 +0300

    First Commit

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onresize = screen;
+window.onload = screen;
+
+function screen () {
+    myWidth = window.innerWidth;
+    document.getElementById('size').innerHTML
+    = "width: " + myWidth + "px";
+}
